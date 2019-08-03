bf64fa999a8c7f008471ad75fa8d96c395c11f01
AmrMonzir <amrmonzirr@gmail.com>
2019-08-03 12:27:54 +0300

    Added a tick method to TankPlayerController and made the grounds for AimTowardsCrosshair()

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -12,8 +12,23 @@ void ATankPlayerController::BeginPlay()
 		UE_LOG(LogTemp, Warning, TEXT("Got a possessed tank %s"), *GetControlledTank()->GetName());
 	}
 }
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	//Aim at crosshair
+	AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller Ticking"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() {
+
+	if (!GetControlledTank()) { return; }
+
+	//Get world location if linetrace through crosshair
+	//if it hits the landscape
+		//tell controlled tank to aim at this point
 }
